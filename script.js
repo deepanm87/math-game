6ad3c469cb19835e56cf92f9833b6b82d6ade3a5
deepanm87 <deepan.murugan29@gmail.com>
2025-04-02 12:55:51 -0400

    generate random equations

--- script.js
+++ script.js
@@ -24,55 +24,60 @@ const wrongFormat = []
 
 // Scroll
 
-// Create Correct/Incorrect Random Equations
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
 function createEquations() {
-  // Randomly choose how many correct equations there should be
-  // const correctEquations = 
-  // Set amount of wrong equations
-  // const wrongEquations = 
-  // Loop through, multiply random numbers up to 9, push to array
-  // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
-  //   equationObject = { value: equation, evaluated: 'true' };
-  //   equationsArray.push(equationObject);
-  // }
-  // Loop through, mess with the equation results, push to array
-  // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
-  //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
-  //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
-  //   const equation = wrongFormat[formatChoice];
-  //   equationObject = { value: equation, evaluated: 'false' };
-  //   equationsArray.push(equationObject);
-  // }
+  
+  const correctEquations = getRandomInt(questionAmount)
+  const wrongEquations = questionAmount - correctEquations
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9)
+    secondNumber = getRandomInt(9)
+    const equationValue = firstNumber * secondNumber
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`
+    equationObject = { value: equation, evaluated: 'true' }
+    equationsArray.push(equationObject)
+  }
+   
+  for (let i = 0; i < wrongEquations; i++) {
+     firstNumber = getRandomInt(9)
+     secondNumber = getRandomInt(9)
+     const equationValue = firstNumber * secondNumber;
+     wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`
+     wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`
+     wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`
+     const formatChoice = getRandomInt(3)
+     const equation = wrongFormat[formatChoice]
+     equationObject = { 
+        value: equation, 
+        evaluated: 'false' 
+      }
+     equationsArray.push(equationObject)
+  }
+  shuffle(equationsArray)
 }
 
 // Dynamically adding correct/incorrect equations
 // function populateGamePage() {
 //   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
+//   itemContainer.textContent = ''
 //   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
+//   const topSpacer = document.createElement('div')
+//   topSpacer.classList.add('height-240')
 //   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
+//   const selectedItem = document.createElement('div')
+//   selectedItem.classList.add('selected-item')
 //   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+//   itemContainer.append(topSpacer, selectedItem)
 
 //   // Create Equations, Build Elements in DOM
 
 //   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
+//   const bottomSpacer = document.createElement('div')
+//   bottomSpacer.classList.add('height-500')
+//   itemContainer.appendChild(bottomSpacer)
 // }
 
 function countdownStart() {
@@ -92,6 +97,7 @@ function showCountdown() {
   countdownPage.hidden = false
   splashPage.hidden = true
   countdownStart()
+  createEquations()
 }
 
 function getRadioValue() {
